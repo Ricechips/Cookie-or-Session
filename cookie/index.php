4f62a391defee50838560e38c31017d6ee330864
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-21 11:46:40 +0800

    Update index.php

--- cookie/index.php
+++ cookie/index.php
@@ -1,3 +1,23 @@
 <?php
 
 setcookie('name','Ricechips');
+setcookie('h','hello');
+// header('Location:a.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    
+    <script>
+      alert(document,cookie);
+    </script>
+  </head>
+<body>
+
+  </body>
+</html>
